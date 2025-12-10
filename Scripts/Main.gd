0bce99abbf76184fc07442824793611a0849b1b9
Iru21 <mateuszsamsel3@gmail.com>
2025-12-10 15:49:11 +0100

    Ensure there is not more than one camera zone manager in a scene

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -2,6 +2,14 @@ extends Node
 
 static var debug : Debug
 
+func _ready() -> void:
+	var zone_manager_count := 0
+	for node in find_children("*"):
+		if node is CameraZoneManager:
+			zone_manager_count += 1
+
+	assert(zone_manager_count <= 1, "There should be not more than one CameraZoneManager in the scene.")
+				
 func _process(_delta: float) -> void:
 	debug.add_debug_property("FPS", Engine.get_frames_per_second(), 60)
 
