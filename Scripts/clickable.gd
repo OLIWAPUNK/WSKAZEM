7239e7278df7d754c0bbd851901bb0aaa745ae30
Szczoor <mateusz.gorniak8@gmail.com>
2025-12-17 20:37:55 +0100

    Outline shader on hover for NPC

--- Scripts/clickable.gd
+++ Scripts/clickable.gd
@@ -2,22 +2,37 @@ extends Node
 
 @onready var parent := $".."
 
+var mesh : MeshInstance3D
+
 
 
 func _ready() -> void:
 	
+	for child in parent.get_children():
+
+		if child is not MeshInstance3D: 
+			continue
+
+		mesh = child
+
+	print(mesh)
 	parent.connect("input_event", clickable_clicked)
+	parent.connect("mouse_entered", on_hover)
+	parent.connect("mouse_exited", on_unhover)
 
 
 func on_hover() -> void:
 
-	pass
+	mesh.material_overlay = Global.overlay_outline_material
+
+
+func on_unhover() -> void:
+
+	mesh.material_overlay = null
 
 
 func clickable_clicked(_camera: Node, _event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 
-	on_hover()
-	
 	if Input.is_action_just_pressed("go_to_point"):
 
 		print(parent)
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,8 +1,15 @@
 extends Node
 
 static var debug : Debug
+
+@export var overlay_outline_material : ShaderMaterial
 		
 
 
+func _ready() -> void:
+	assert(overlay_outline_material, "No overlay outline material set")
+
+
 func _process(_delta: float) -> void:
+	
 	debug.add_debug_property("FPS", Engine.get_frames_per_second(), 60)
