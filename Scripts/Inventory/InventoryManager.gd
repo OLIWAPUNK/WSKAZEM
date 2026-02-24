c7837d6be4b47ab5e843031deb80a60309efee2f
Iru21 <yuridementia0@gmail.com>
2026-02-24 22:02:03 +0100

    Use button for inventory slot instead

--- Scripts/Inventory/InventoryManager.gd
+++ Scripts/Inventory/InventoryManager.gd
@@ -2,46 +2,38 @@ class_name InventoryManager
 extends Node
 
 @onready var item_mesh: MeshInstance3D = %ItemMesh
-@onready var panel: Panel = $"../SlotContainer/Panel"
+@onready var button: Button = $"../SlotContainer/Button"
 
 # var held_item: ItemData = null
 var held_item: ItemData = preload("res://Resources/Items/RockItem.tres")
 
 func _ready() -> void:
-	panel.gui_input.connect(_on_panel_gui_input)
-	panel.mouse_entered.connect(_on_panel_mouse_entered)
+	button.pressed.connect(_on_button_pressed)
 
-	if held_item:
-		_set_mesh(held_item.mesh)
-	else:
-		_clear_mesh()
+	_set_held_item(held_item)
 
-func _on_panel_gui_input(event: InputEvent) -> void:
-	print_debug("dgmervnewkvbgwlk")
-	if event is InputEventMouse and event.is_action_just_pressed("mouse_interact"):
-		drop()
-
-		get_viewport().set_input_as_handled()
-
-func _on_panel_mouse_entered() -> void:
-	print_debug("dgmervnewkvbgwlk")
-
-func _set_mesh(mesh: Mesh):
-	item_mesh.mesh = mesh
-	item_mesh.visible = mesh != null
-
-func _clear_mesh():
-	item_mesh.mesh = null
-	item_mesh.visible = false
+func _on_button_pressed() -> void:
+	drop()
 
 func grab(object: CanBeGrabbed):
 	var item_object: Item = object.parent
 	if held_item:
 		drop()
-	held_item = item_object.item_data
-	_set_mesh(held_item.mesh)
+	_set_held_item(item_object.item_data)
 	item_object.queue_free()
 
 func drop():
-	held_item = null
-	_clear_mesh()
+	_set_held_item(null)
+
+func _set_held_item(item: ItemData):
+	held_item = item
+	if held_item:
+		item_mesh.mesh = held_item.mesh
+		item_mesh.visible = true
+		button.disabled = false
+		button.tooltip_text = "Drop held item"
+	else:
+		item_mesh.mesh = null
+		item_mesh.visible = false
+		button.disabled = true
+		button.tooltip_text = ""
