6b984045cc30baa82e05fe5a8bdc3755e1739c62
Iru21 <yuridementia0@gmail.com>
2026-02-25 19:20:35 +0100

    Move drop function to dropped items manager

--- Scripts/Inventory/DroppedItemsManager.gd
+++ Scripts/Inventory/DroppedItemsManager.gd
@@ -1,6 +1,40 @@
 class_name DroppedItemsManager
 extends Node3D
 
+const DROPPED_ITEM_SCENE = preload("res://Scenes/GameWorld/Items/DroppedItem.tscn")
+const CAN_BE_GRABBED_SCENE = preload("res://Scenes/GameWorld/Traits/CanBeGrabbed.tscn")
+
 func _ready() -> void:
 	assert(Global.dropped_items_manager == null, "There should only be one DroppedItemsManager in the scene")
 	Global.dropped_items_manager = self
+
+func drop(held_item: ItemData):
+	var ray_directions = [
+		Vector3(0, 0, 1),
+		Vector3(0, 0, -1),
+		Vector3(1, 0, 0),
+		Vector3(-1, 0, 0),
+	].map(func (dir): return dir.rotated(Vector3.UP, Global.player.global_rotation.y)) as Array[Vector3]
+	var player = Global.player
+	var space_state = player.get_world_3d().direct_space_state
+	var from = player.global_transform.origin
+	var valid_direction = Vector3.ZERO
+	for direction in ray_directions:
+		var to = from + direction
+		to.y += 1
+		var query = PhysicsRayQueryParameters3D.create(from, to)
+		var result = space_state.intersect_ray(query)
+		if not result:
+			valid_direction = to
+			break
+	if valid_direction == Vector3.ZERO:
+		return false
+	var item_scene = load(held_item.scene_path)
+	var dropped_item: RigidBody3D = DROPPED_ITEM_SCENE.instantiate()
+	add_child(dropped_item)
+	var item = item_scene.instantiate() as Item
+	dropped_item.add_child(item)
+	item.add_child(CAN_BE_GRABBED_SCENE.instantiate())
+	dropped_item.global_transform.origin = valid_direction
+	dropped_item.linear_velocity = (valid_direction - from).normalized() * 3 + Vector3.DOWN * 2
+	return true
--- Scripts/Inventory/InventoryManager.gd
+++ Scripts/Inventory/InventoryManager.gd
@@ -4,9 +4,6 @@ extends Node
 @onready var item_mesh: MeshInstance3D = %ItemMesh
 @onready var button: Button = $"../SlotContainer/Button"
 
-const DROPPED_ITEM_SCENE = preload("res://Scenes/GameWorld/Items/DroppedItem.tscn")
-const CAN_BE_GRABBED_SCENE = preload("res://Scenes/GameWorld/Traits/CanBeGrabbed.tscn")
-
 var held_item: ItemData = null
 # var held_item: ItemData = preload("res://Resources/Items/RockItem.tres")
 
@@ -26,37 +23,8 @@ func grab(object: CanBeGrabbed):
 	item_object.get_parent_node_3d().queue_free()
 
 func drop():
-	var ray_directions = [
-		Vector3(0, 0, 1),
-		Vector3(0, 0, -1),
-		Vector3(1, 0, 0),
-		Vector3(-1, 0, 0),
-	].map(func (dir): return dir.rotated(Vector3.UP, Global.player.global_rotation.y)) as Array[Vector3]
-	var player = Global.player
-	var space_state = player.get_world_3d().direct_space_state
-	var from = player.global_transform.origin
-	var valid_direction = Vector3.ZERO
-	for direction in ray_directions:
-		var to = from + direction
-		to.y += 1
-		var query = PhysicsRayQueryParameters3D.create(from, to)
-		var result = space_state.intersect_ray(query)
-		if not result:
-			valid_direction = to
-			break
-	if valid_direction == Vector3.ZERO:
-		return
-	var item_scene = load(held_item.scene_path)
-	var dropped_item: RigidBody3D = DROPPED_ITEM_SCENE.instantiate()
-	Global.dropped_items_manager.add_child(dropped_item)
-	var item = item_scene.instantiate() as Item
-	dropped_item.add_child(item)
-	item.add_child(CAN_BE_GRABBED_SCENE.instantiate())
-	dropped_item.global_transform.origin = valid_direction
-	dropped_item.linear_velocity = (valid_direction - from).normalized() * 3 + Vector3.DOWN * 2
-	_set_held_item(null)
-	
-
+	if Global.dropped_items_manager.drop(held_item):
+		_set_held_item(null)
 
 func _set_held_item(item: ItemData):
 	held_item = item
