8a6e99a5bb21134247f6a7c7368b1f2984840624
Iru21 <yuridementia0@gmail.com>
2026-03-08 01:15:36 +0100

    Fix invalid settings on load

--- Scripts/Settings.gd
+++ Scripts/Settings.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const SETTINGS_FILE_PATH: String = "user://settings.cfg"
+
 var _default_settings: Dictionary = {
 	"video": {
 		"resolution": Vector2i(1280, 720),
@@ -16,19 +18,37 @@ var _default_settings: Dictionary = {
 var _settings: Dictionary = _default_settings.duplicate(true)
 
 func _ready() -> void:
-	if not FileAccess.file_exists("user://settings.cfg"):
+	if not FileAccess.file_exists(SETTINGS_FILE_PATH):
 		_save_settings()
 	else:
 		_load_settings()
 
 func _load_settings() -> void:
+	var temp_settings = {}
 	var config = ConfigFile.new()
-	if config.load("user://settings.cfg") == OK:
+	if config.load(SETTINGS_FILE_PATH) == OK:
 		for category in _settings.keys():
+			if not temp_settings.has(category):
+				temp_settings[category] = {}
 			for setting in _settings[category].keys():
 				if config.has_section_key(category, setting):
 					var value = config.get_value(category, setting)
-					_settings[category][setting] = value
+					temp_settings[category][setting] = value
+
+	var updated = false
+	for category in _default_settings.keys():
+		if not temp_settings.has(category) or typeof(temp_settings[category]) != TYPE_DICTIONARY:
+			temp_settings[category] = _default_settings[category].duplicate(true)
+			updated = true
+		else:
+			for setting in _default_settings[category].keys():
+				if not temp_settings[category].has(setting) or typeof(temp_settings[category][setting]) != typeof(_default_settings[category][setting]):
+					temp_settings[category][setting] = _default_settings[category][setting]
+					updated = true
+
+	_settings = temp_settings
+	if updated:
+		_save_settings()
 
 func _save_settings() -> void:
 	var config = ConfigFile.new()
@@ -36,7 +56,7 @@ func _save_settings() -> void:
 		for setting in _settings[category].keys():
 			var value = _settings[category][setting]
 			config.set_value(category, setting, value)
-	config.save("user://settings.cfg")
+	config.save(SETTINGS_FILE_PATH)
 
 func get_setting(category: String, setting: String) -> Variant:
 	if _settings.has(category) and _settings[category].has(setting):
@@ -64,4 +84,3 @@ func compare_settings(other_settings: Dictionary) -> bool:
 			if _settings[category][setting] != other_settings[category][setting]:
 				return false
 	return true
-
