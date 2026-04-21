c249646f7820ef4f3660a3ae9d7b83d0861e6179
Szczoor <mateusz.gorniak8@gmail.com>
2026-04-21 18:34:29 +0200

    delete things (not working)

---
+++ Scripts/PuzzleManagement/ProgressTracker.gd
@@ -0,0 +1,5 @@
+@icon("res://assets/Textures/EditorIcons/ProgressTracker.svg")
+class_name ProgressTracker
+extends Node
+
+
--- Scripts/PuzzleManagement/StateMachine/Transceiver/TransmitterDescription.gd
+++
@@ -1,5 +0,0 @@
-class_name TransmitterDescription
-extends Resource
-
-
-@export_multiline var description: String
