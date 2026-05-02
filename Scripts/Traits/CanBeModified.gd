0d7009b0e6311a396e574349ee45b081f9469798
Iru21 <mateuszsamsel3@gmail.com>
2026-05-02 16:12:14 +0200

    start can be modified

--- Scripts/Traits/CanBeModified.gd
+++ Scripts/Traits/CanBeModified.gd
@@ -1,2 +1,12 @@
+@icon("res://assets/Textures/EditorIcons/Modifiable.svg")
 class_name CanBeModified
 extends CanBeClicked
+
+@export var required_item_identifier: String = ""
+
+func _init() -> void:
+	overlay_outline_material = preload("res://assets/Materials/ModifiableOutline.tres")
+
+func _ready():
+	super._ready()
+	assert(required_item_identifier != "", "%s doesn't have item identifier set!" % name)
