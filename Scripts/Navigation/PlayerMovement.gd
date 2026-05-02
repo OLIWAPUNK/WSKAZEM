1955b0bc7cfbcb807152f84d0b58d2979b3ed25b
Iru21 <yuridementia0@gmail.com>
2026-05-02 11:43:24 +0200

    cleanup or smth idk

--- Scripts/Navigation/PlayerMovement.gd
+++ Scripts/Navigation/PlayerMovement.gd
@@ -1,6 +1,8 @@
 extends Node
 
 @onready var player: CharacterBody3D = $"../PlayerBody"
+@onready var animation_tree: AnimationTree = player.get_node("BaseCharacter/AnimationTree")
+@onready var navigation_agent: NavigationAgent3D = %PlayerNode/NavigationManager.navigation_agent
 @onready var pointer_manager: PointerManager = %PointerManager
 
 @export var ROTATION_SPEED := 10.0
@@ -10,9 +12,6 @@ extends Node
 @onready var target_rotation: Vector3 = player.rotation_degrees
 @onready var gravity: float = (ProjectSettings.get_setting("physics/3d/default_gravity") 
 		* GRAVITY_MULTIPLIER)
-
-var direction: Vector3 = Vector3.ZERO
-var last_frame_held = false
 
 func _ready() -> void:
 	assert(player, "Player node not found")
@@ -33,25 +32,29 @@ func _physics_process(delta):
 	Global.debug.add_debug_property("Player Rotation Degrees", player.global_rotation_degrees.y, 1)
 	Global.debug.add_debug_property("Player Rotation Vector", player.global_rotation.y, 1)
 
-	if Global.map_manager.current_scene != null and not player.is_on_floor():
-		player.velocity.y -= gravity * delta
-	
-	if not %PlayerNode/NavigationManager.navigation_agent.is_navigation_finished():
-		var targetPos = %PlayerNode/NavigationManager.navigation_agent.get_next_path_position()
-		direction = player.global_position.direction_to(targetPos)
-		player.velocity = direction * MOVEMENT_SPEED
-
-	var player_tree: AnimationTree = player.get_node("BaseCharacter/AnimationTree")
 	if (player.velocity.length_squared() > 0.1):
-		var target_angle = atan2(direction.x, direction.z)
+		var target_angle = atan2(player.velocity.x, player.velocity.z)
 		target_rotation.y = lerp_angle(player.rotation.y, target_angle, ROTATION_SPEED * delta)
 
-		player_tree["parameters/Transition/transition_request"] = "walk"
+		animation_tree["parameters/Transition/transition_request"] = "walk"
 	else:
-		player_tree["parameters/Transition/transition_request"] = "idle"
+		animation_tree["parameters/Transition/transition_request"] = "idle"
 	player.rotation = target_rotation
 
+	if Global.map_manager.current_scene == null:
+		return
+	if NavigationServer3D.map_get_iteration_id(navigation_agent.get_navigation_map()) == 0:
+		return
+
+	if not player.is_on_floor():
+		player.velocity.y -= gravity * delta
+	
+	if not navigation_agent.is_navigation_finished():
+		var next_pos = navigation_agent.get_next_path_position()
+		player.velocity = player.global_position.direction_to(next_pos) * MOVEMENT_SPEED
+	
 	player.move_and_slide()
+	
 
 func on_save():
 	Saves.set_data("player.position", {
